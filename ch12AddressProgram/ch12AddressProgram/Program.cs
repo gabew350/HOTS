ef169967342927dc79da2c1b47524e8d00d56db7
Gabe Williams <Gabriel_Williams@ranken.org>
2021-03-17 10:42:09 -0500

    HOT 12

---
+++ ch12AddressProgram/ch12AddressProgram/Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ch12AddressProgram
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            static void Main()
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            const int NUMCUSTOMERS = 5;
+
+            List<Account> accounts = new List<Account>();
+
+            int validAcctNumLoc = -1;
+            int validPinNumLoc = -1;
+            bool yourLogginIn = false;
+
+            private void buttonLogin_Click(object sender, EventArgs e)
+            {
+                Login();
+            }
+            private void Login()
+            {
+                bool missingInfo = true;
+                bool badAcctNumber = true;
+                bool badPinNumber = true;
+
+                missingInfo = isAnyLoginInfoMissing();
+                if (missingInfo)
+                {
+                    return;
+                }
+
+                badAcctNumber = validateAccountNumber(textBoxAccountNumber.text);
+
+                badPinNumber = validatePinNumber(textBoxpinNumber.text);
+
+                if ((badAcctNumber) || (badPinNumber))
+                {
+                    displayBadAccountNumberOrPinNumberMessage();
+                    return;
+                }
+
+                if (validAcctNumLoc != validPinNumberLoc)
+                {
+                    displayBadAccountNumberOrPinNumberMessage();
+                    return;
+                }
+                rewriteWelcomeScreen();
+                makeScreenActive();
+
+            }
+
+            public void makeScreenActive()
+            {
+                yourLogginIn = true;
+                buttonLogin.Enabled = false
+                buttonLogout.Enabled = true;
+                buttonDeposit.Enabled = true;
+                buttonWithdrawal.Enabled = true;
+            }
+            private bool isAnyLoginInfoMissing()
+            {
+                textBoxWelcomeInfo.text = "";
+                string MissingLoginStr = "";
+                MissingLoginStr += "";
+                MissingLoginStr += "";
+                MissingLoginStr += "";
+                MissingLoginStr += "";
+
+                if ((textBoxAccountNumber.text == "") ||
+                    (textBoxPinNumber.text == ""))
+                {
+                    textBoxWelcomeInfo.Text = "";
+                    textBoxWelcomeInfo.text == MissingLoginStr;
+
+                    clearLoginFields();
+                    return true;
+                }
+
+                return true;
+            }
+
+            private bool ValidateAccountNumber(string an)
+            {
+                validAcctNumLoc = -1;
+
+                for (int lcv = 0; lcv < accounts.Count; ++lcv)
+                {
+                    if (an == accounts[lcv].GetAccountNumber())
+                    {
+                        validAcctNumLoc = lcv;
+                    }
+                    return false
+            }
+                private bool ValidatePinNumber(string an)
+                {
+                    validAcctNumLoc = -1;
+
+                    for (int lcv = 0; lcv < accounts.Count; ++lcv)
+                    {
+                        if (pn == accounts[lcv].GetPin())
+                        {
+                            validPinNumLoc = lcv;
+                            return false;
+                        }
+                        return true;
+                    }
+
+                    private void displayBadAccountsNumberOrPinNumberMessage()
+                    {
+                        string wrongLoginStr = "";
+                        wrongLoginStr += "";
+                        wrongLoginStr += "";
+
+                        textBoxWelcomeInfo.Text = "";
+                        TextBoxWelcomeInfo.Text = wrongLoginStr;
+
+                        clearLoginFields();
+                    }
+                    private void clearLoginFields()
+                    {
+                        textBoxAccountNumber.Text = "";
+                        textBoxPinNumber
+    
+
+                }
+
+                    private void reWriteWelcomeScreen()
+                    {
+                        string WelcomeStr += "welcome";
+                        WelcomeStr accounts tthexdbuton
+                    }
+                    bool keepGoing = loggedInOrNot();
+                    if (keepGoing)
+                    {
+                        attemptTheDeposit();
+                        return;
+                    }
+
+                    public void attemptTheDeposit()
+                    {
+                        decimal deposit;
+
+                        try
+                        {
+                            deposit = Convert.ToDecimal(textBoxDeposit.text);
+                            accounts[validPinNumLoc].MakeDeposit(deposit);
+                            reWriteWelcomeScreen();
+
+                        }
+                        catch (Exception e)
+                        {
+                            return
+                        }
+                        attemptTheDepositwithdraw()
+                            ithdraw
+                    }
+                    private bool loggedInOtNot()
+                    {
+                        if (!youreLogginIn)
+                        {
+                            textBoxWelcome
+                        }
+                    }
+                    private void buttonLogout_Click(object sender, EventArgs e)
+                    {
+                        Logout();
+                    }
+                    private void Logout()
+                    {
+                        if (MessageBox.Show("do you really want to log out",
+                            "logout now?",
+                            MessageBoxButtons.YesNo,
+                            MessageBoxIcons) == dialog result.yes)
+                            {
+                blankout all thing on screen();
+                yourLogginIn
+                    buttons
+            }
+            textBoxWelcometextprivateVoid
+                }
+
+    }
+}
+
+
---
+++ test12Program/test12Program/Class1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace password
+{
+    class password
+    {
+        public decimal MINIMUMNALANCE = 25.00m;
+
+        private readonly string _title;
+        private readonly string _firstName;
+        private readonly string _lastName;
+        private readonly string _accountNumber;
+        private readonly string _pin;
+        private decimal _balance;
+
+        public password(string title,
+                        string firstName,
+            string lastName,
+            string accountNumber,
+            string pin,
+            decimal balance)
+        {
+            _title = title;
+            _firstName = firstName;
+            _lastName = lastName;
+            _accountNumber = accountNumber;
+            _pin = pin;
+            _balance = balance;
+        }
+        public string GetTitle()
+        {
+            return _title;
+        }
+
+        public string GetFirstName()
+        {
+            return _firstName;
+        }
+
+        public string GetLastName()
+        {
+            return _lastName;
+        }
+
+        public string GetAccountNumber()
+        {
+            return _accountNumber;
+        }
+
+        public string GetPin()
+        {
+            return _pin;
+        }
+
+        public string GetBalance()
+        {
+            return _balance;
+        }
+
+        public void MakeDeposit(decimal deposit)
+        {
+            decimal theDeposit = -1m;
+
+            try
+            {
+
+                try
+                {
+                    theDeposit = deposit;
+
+                    if (theDeposit <= 0)
+                    {
+                        MessageBox.Show("Illegal Deposit ( 0 or negative Amount) Attempted!",
+                            "Illegal Deposit",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    _balance += deposit;
+                }
+                catch (Exception e)
+                {
+                    return;
+                }
+            }
+
+
+        public void MakeWithdrawl(decimal amount)
+            {
+                decimal theAmount = amount;
+
+                if (amount <= 0)
+                {
+                    MessageBox.Show("Illegal Withdrawl ( 0 or negative Amount) Attempted!",
+                           "Illegal Withdrawl",
+                           MessageBoxButtons.OK,
+                           MessageBoxIcon.Error);
+                    return;
+                }
+
+                temp = _balance - theAmount;
+                if (temp >= MINIMUMNALANCE)
+                {
+                    _balance -= amount;
+                }
+
+                else
+                {
+                    MessageBox.Show("Illegal Withdrawl ( 0 or negative Amount) Attempted!",
+                          "Illegal Withdrawl",
+                          MessageBoxButtons.OK,
+                          MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+
+        }
+
+
+    }
+}
